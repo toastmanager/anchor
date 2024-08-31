54a4a36405363d92887230175614b4b8903703f8
toastmanager <mrbonkyou@gmail.com>
2024-08-31 21:47:55 +0900

    colorScheme & textTheme & filledButtonTheme & outlinedButtonTheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,57 @@
-import 'package:anchor/app.dart';
-import 'package:anchor/firebase_options.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:smooth_corner/smooth_corner.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
-  FirebaseFirestore.instance.settings = const Settings(
-    persistenceEnabled: true,
-  );
 
   runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Anchor',
+        theme: ThemeData(
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: const Color(0xFF554AF0)).copyWith(
+            primary: const Color(0xFF554AF0),
+          ),
+          textTheme: Theme.of(context)
+              .textTheme
+              .apply(fontSizeDelta: 2, fontFamily: 'Onest'),
+          filledButtonTheme: FilledButtonThemeData(
+              style: ButtonStyle(
+                  padding: const WidgetStatePropertyAll(
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 16)),
+                  shape: WidgetStatePropertyAll(SmoothRectangleBorder(
+                    smoothness: 1,
+                    borderRadius: BorderRadius.circular(12),
+                  )))),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+              style: ButtonStyle(
+                  padding: const WidgetStatePropertyAll(
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 16)),
+                  shape: WidgetStatePropertyAll(SmoothRectangleBorder(
+                      smoothness: 1, borderRadius: BorderRadius.circular(12))),
+                  foregroundColor: WidgetStatePropertyAll(
+                      Theme.of(context).colorScheme.onSurface))),
+        ),
+        home: Scaffold(
+          body: SafeArea(
+              child: SingleChildScrollView(
+            child: Column(
+              children: [
+                OutlinedButton(
+                    onPressed: () {}, child: const Text('Зарегистрироваться')),
+                FilledButton(onPressed: () {}, child: const Text('Войти')),
+              ],
+            ),
+          )),
+        ));
+  }
 }
