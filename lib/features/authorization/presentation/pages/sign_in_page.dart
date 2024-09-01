df8ea57624215a189285aead0418b94bdfbf1799
toastmanager <mrbonkyou@gmail.com>
2024-09-01 10:25:13 +0900

    password textfield for sign_in_page

--- lib/features/authorization/presentation/pages/sign_in_page.dart
+++ lib/features/authorization/presentation/pages/sign_in_page.dart
@@ -1,3 +1,4 @@
+import 'package:anchor/core/components/eye_toggle.dart';
 import 'package:anchor/core/components/squircle_clipper.dart';
 import 'package:anchor/core/theme/app_theme.dart';
 import 'package:auto_route/annotations.dart';
@@ -12,6 +13,8 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  bool isPasswordObscure = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,8 +25,17 @@ class _SignInPageState extends State<SignInPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text('Sign in'),
+            const SizedBox(
+              height: 10,
+            ),
             Text('Enter your account details'),
+            const SizedBox(
+              height: 20,
+            ),
             Text('Email'),
+            const SizedBox(
+              height: 10,
+            ),
             SquircleClipper(
               side: AppTheme.borderSide(context),
               child: TextField(
@@ -33,7 +45,42 @@ class _SignInPageState extends State<SignInPage> {
                 ),
               ),
             ),
+            const SizedBox(
+              height: 10,
+            ),
             Text('Password'),
+            const SizedBox(
+              height: 10,
+            ),
+            SquircleClipper(
+              side: AppTheme.borderSide(context),
+              child: TextField(
+                obscureText: isPasswordObscure,
+                decoration: AppTheme.inputDecoration(context,
+                    hintText: 'Enter your password',
+                    suffix: Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: EyeToggleButton(
+                        isOpen: isPasswordObscure,
+                        onTap: () => setState(() {
+                          isPasswordObscure = !isPasswordObscure;
+                        }),
+                      ),
+                    )),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Forgot your password?'),
+                ),
+              ],
+            )
           ],
         ),
       ),
